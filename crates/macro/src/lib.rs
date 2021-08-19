fb4b4e209378c08b9da12d1f5a8dc4803c16ff1c
Danilo Guanabara <danilo@sensorial.systems>
2021-08-19 12:24:26 -0300

    Updating structure

--- crates/macro/src/lib.rs
+++
@@ -1,3 +0,0 @@
-//! Generator macro.
-
-// ligen::define_project_generator!(name = "ligen_cmake", generator = "ligen_cmake_core::Generator");
--- crates/main/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub use ligen_cmake_core as core;
-pub use ligen_cmake_macro as proc_macro;
-
-pub use ligen_cmake_macro::*;
-pub use ligen_cmake_core::*;
